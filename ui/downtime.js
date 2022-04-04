7bccfa7214b608e1fba46c91c00d3d41ecb4052c
Scorpyy1 <s.lattanzio@yahoo.com>
2022-04-04 16:15:36 +0200

    Add files via upload

---
+++ ui/downtime.js
@@ -0,0 +1,24 @@
+module.exports = {
+		
+				  "serviceInstanceId": "fortnite",
+				  "status": "UP",
+				  "message": "ur gay",
+				  "maintenanceUri": null,
+				  "overrideCatalogIds": [
+					"a7f138b2e51945ffbfdacc1af0541053"
+				  ],
+				  "allowedActions": [
+					"PLAY",
+					"DOWNLOAD"
+				  ],
+				  "banned": false,
+				  "launcherInfoDTO": {
+					"appName": "Fortnite",
+					"catalogItemId": "4fe75bbc5a674f4f9b356b5c90567da5",
+					"namespace": "fn"
+				  },
+          
+          
+	};
+
+// prob the best
